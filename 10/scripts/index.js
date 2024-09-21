e6089563fec6a51ecc17abe519f09a7165dd0178
KarinaDemchenko02 <146772294+KarinaDemchenko02@users.noreply.github.com>
2024-09-21 13:51:59 +0000

    Deploying to gh-pages from @ KarinaDemchenko02/2558209-cat-energy-32@f55a1ba3d522b4b91245536f0b9fea8761140b42 🚀

--- 10/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var e=document.querySelector(".button-burger"),t=document.querySelector(".nav");e.addEventListener("click",()=>{e.classList.toggle("button-burger--close"),t.classList.toggle("nav--open")});
